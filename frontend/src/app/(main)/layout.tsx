9c4ed5c07ddfe8e6ba2605b087a1c8d3f6ddde94
Ankit <devxankit@gmail.com>
2026-05-30 17:38:53 +0530

    CTA button fixed

--- frontend/src/app/(main)/layout.tsx
+++ frontend/src/app/(main)/layout.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { FAB } from "@/components/FAB";
+import { PageTransition } from "@/components/PageTransition";
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      {children}
+      <PageTransition>{children}</PageTransition>
       <Footer />
       <FAB />
     </>
